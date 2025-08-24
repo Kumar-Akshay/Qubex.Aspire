7fb4b43f1ebb69622c4954c462cb20b6baed5f89
Kumar-Akshay <Akshaymalhi012@gmail.com>
2025-08-24 17:13:51 +0100

    aspire setup with different service

--- Qubex.Aspire/AppHost.cs
+++ Qubex.Aspire/AppHost.cs
@@ -1,5 +1,29 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 
+var postgresDb = builder.AddPostgres("postgres")
+    .WithEnvironment("POSTGRES_DB", "myDatabase")
+    .AddDatabase("myDatabase");
 
-builder.Build().Run();
+var mongoDb = builder.AddMongoDB("mongodb")
+    .WithEnvironment("MONGODB", "myMongodb")
+    .AddDatabase("myMongodb");
+
+var kafkaDb = builder.AddKafka("kafka")
+    .WithEnvironment("KAFKA", "myKafka");
+
+// var cosmosDb = builder.AddAzureCosmosDB( "myAzureCosmosDB")
+//     .AddCosmosDatabase("myCosmosDB");
+
+
+
+
+
+var redis = builder.AddRedis("Cache");
+var api = builder.AddProject<Qubex_API>("Qubex-WebAPI");
+var tracking = builder.AddProject<Qubex_TrackingAPI>("Qubex-TrackingAPI");
+
+builder.
+    Build().Run();
